e2a8554d63844de91ae5b7c611a58ba6be5b9939
Cubit Packaging <hello@cubitpackaging.com>
2025-07-16 00:11:03 +0500

    Update footer social media icons to use SVG files with white styling
    - Replace inline SVG icons with external SVG files from public folder
    - Add Pinterest and TikTok social media links
    - Apply white color styling with CSS filters for consistent appearance
    - Maintain hover effects with opacity and scale transitions
    - Improve code maintainability by using Next.js Image component

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import Image from 'next/image'
 import PolicyModal from './PolicyModal'
 import RushOrderModal from './RushOrderModal'
 
@@ -32,29 +33,27 @@ const Footer = () => {
     {
       name: 'Instagram',
       href: 'https://instagram.com/cubitpackaging',
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-          <path fillRule="evenodd" d="M12.017 0C8.396 0 7.929.013 6.71.072 5.493.131 4.67.333 3.953.63a5.612 5.612 0 00-2.051 1.303 5.612 5.612 0 00-1.303 2.051C.333 4.67.131 5.493.072 6.71.013 7.929 0 8.396 0 12.017c0 3.624.013 4.09.072 5.31.059 1.217.261 2.04.558 2.757.298.718.686 1.329 1.303 2.051.722.617 1.333 1.005 2.051 1.303.717.297 1.54.499 2.757.558 1.22.059 1.686.072 5.31.072 3.624 0 4.09-.013 5.31-.072 1.217-.059 2.04-.261 2.757-.558a5.612 5.612 0 002.051-1.303c.617-.722 1.005-1.333 1.303-2.051.297-.717.499-1.54.558-2.757.059-1.22.072-1.686.072-5.31 0-3.624-.013-4.09-.072-5.31-.059-1.217-.261-2.04-.558-2.757a5.612 5.612 0 00-1.303-2.051A5.612 5.612 0 0019.747.63C19.03.333 18.207.131 16.99.072 15.771.013 15.304 0 11.683 0h.334zm-.717 1.442h.718c3.136 0 3.554.012 4.81.07 1.160.053 1.79.249 2.207.412.555.216.95.475 1.365.89.415.415.674.81.89 1.365.163.417.359 1.047.412 2.207.058 1.256.07 1.674.07 4.81 0 3.136-.012 3.554-.07 4.81-.053 1.16-.249 1.79-.412 2.207-.216.555-.475.95-.89 1.365-.415.415-.81.674-1.365.89-.417.163-1.047.359-2.207.412-1.256.058-1.674.07-4.81.07-3.136 0-3.554-.012-4.81-.07-1.16-.053-1.79-.249-2.207-.412a3.67 3.67 0 01-1.365-.89 3.67 3.67 0 01-.89-1.365c-.163-.417-.359-1.047-.412-2.207-.058-1.256-.07-1.674-.07-4.81 0-3.136.012-3.554.07-4.81.053-1.16.249-1.79.412-2.207.216-.555.475-.95.89-1.365a3.67 3.67 0 011.365-.89c.417-.163 1.047-.359 2.207-.412 1.256-.058 1.674-.07 4.81-.07l-.718-.07z" />
-        </svg>
-      )
+      iconPath: '/Instagram.svg'
     },
     {
       name: 'LinkedIn',
       href: 'https://linkedin.com/company/cubit-packaging',
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-        </svg>
-      )
+      iconPath: '/LinkedIn.svg'
     },
     {
       name: 'Facebook',
       href: 'https://facebook.com/cubitpackaging',
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-        </svg>
-      )
+      iconPath: '/facebook.svg'
+    },
+    {
+      name: 'Pinterest',
+      href: 'https://pinterest.com/cubitpackaging',
+      iconPath: '/Pintrest.svg'
+    },
+    {
+      name: 'TikTok',
+      href: 'https://tiktok.com/@cubitpackaging',
+      iconPath: '/tiktok.svg'
     }
   ]
 
@@ -221,11 +220,17 @@ const Footer = () => {
                   href={social.href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-gray-400 hover:text-green-400 transition-colors duration-300 p-1"
+                  className="group transition-all duration-300 p-1"
                   aria-label={social.name}
                 >
-                  <div className="w-5 h-5 sm:w-6 sm:h-6">
-                  {social.icon}
+                  <div className="w-5 h-5 sm:w-6 sm:h-6 relative">
+                    <Image
+                      src={social.iconPath}
+                      alt={social.name}
+                      width={24}
+                      height={24}
+                      className="w-full h-full object-contain filter brightness-0 invert opacity-70 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300"
+                    />
                   </div>
                 </a>
               ))}
