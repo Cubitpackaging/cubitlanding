4cb62742a6a45576023738b9355d601e552bbd28
Cubit Packaging <hello@cubitpackaging.com>
2025-09-05 18:13:59 +0500

    Optimize LiveChat widget loading speed - reduce 5 second delay with preload and faster initialization

--- app/layout.js
+++ app/layout.js
@@ -33,10 +33,13 @@ export default function RootLayout({ children }) {
         {children}
         
 
-        {/* LiveChat Script */}
+        {/* Preload LiveChat for faster loading */}
+        <link rel="preload" href="https://cdn.livechatinc.com/tracking.js" as="script" />
+        
+        {/* LiveChat Script - Load earlier for faster appearance */}
         <Script
           id="livechat-script"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.__lc = window.__lc || {};
@@ -44,6 +47,44 @@ export default function RootLayout({ children }) {
               window.__lc.integration_name = "manual_onboarding";
               window.__lc.product_name = "livechat";
               ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+            `
+          }}
+        />
+        
+        {/* LiveChat Fast Initialization */}
+        <Script
+          id="livechat-fast-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              // Force faster LiveChat initialization
+              document.addEventListener('DOMContentLoaded', function() {
+                if (window.LiveChatWidget) {
+                  // Force widget to show immediately
+                  setTimeout(() => {
+                    if (window.LiveChatWidget && window.LiveChatWidget.init) {
+                      window.LiveChatWidget.init();
+                    }
+                  }, 100);
+                }
+              });
+              
+              // Backup initialization for even faster loading
+              if (document.readyState === 'loading') {
+                document.addEventListener('DOMContentLoaded', function() {
+                  setTimeout(() => {
+                    if (window.LiveChatWidget) {
+                      window.LiveChatWidget.call('maximize');
+                    }
+                  }, 500);
+                });
+              } else {
+                setTimeout(() => {
+                  if (window.LiveChatWidget) {
+                    window.LiveChatWidget.call('maximize');
+                  }
+                }, 500);
+              }
             `
           }}
         />
